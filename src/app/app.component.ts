c0c431387194c5f07429caad7adacd080c19c84f
ricajus <ricartoonstudio@gmail.com>
2026-04-21 07:27:26 -0300

    feat: adjust main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'escola';
+
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'logo', // Nome que você vai usar no HTML
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/logo.svg')
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 //Views
 import { AppComponent } from './app.component';
@@ -12,6 +13,20 @@ import { AppRoutingModule } from './app-routing.module';
 
 //Components
 import { HeaderComponent } from "./components/header/header.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Forms
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+//Material components
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { TableComponent } from './components/table/table.component';
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
   declarations: [
@@ -19,11 +34,23 @@ import { HeaderComponent } from "./components/header/header.component";
     TurmaComponent,
     AlunoComponent,
     MatriculaComponent,
-    HeaderComponent
+    HeaderComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
